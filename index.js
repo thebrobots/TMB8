579ce56c4fe28893a32b9b9529b9edbec661f7f4
elttayman <77508640+elttayman@users.noreply.github.com>
2021-04-03 17:41:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,33 @@
 const { Client, Collection } = require("discord.js");
 const client = new Client({ disableMentions: "everyone" });
-const { loadCommands } = require("./commandLoader");
-const { TOKEN, PREFIX } = require("./util/sharkyUtil");
-const express = require("express");
-
-require("./util/eventLoader")(client);
-require("./util/nqn")(client);
-require("./util/muteHelper")(client);
-
-const app = express();
-const cors = require("cors");
-const port = 8080;
 const chalk = require("chalk");
+const { TOKEN, PREFIX } = require("./util/sharkyUtil");
 
-app.use(cors());
+client.on("ready", () => {
 
-app.get("/bot/guilds", (req, res) => {
-  let guild = client.guilds.cache.size;
-  res.json(guild);
-});
+  let statuses = [
+    `revamping`,
+    `new features`,
+    `don't kick me`,
+    `better than ever`,
+  ];
+  
+  let index = 0;
+  let nums = [0, 1, 2, 3];
+  setInterval(() => {
+    client.user.setActivity(statuses[index], { type: "WATCHING" });
+    index = nums[index + 1] ? index + 1 : 0;
+  }, 30000);
 
-app.get("/bot/users", (req, res) => {
-  let members = client.users.cache.size;
-  res.json(members);
-});
+  client.on("warn", (info) => console.log(info));
+  client.on("error", console.error);
 
-app.get("/bot/channels", (req, res) => {
-  let channels = client.channels.cache.size;
-  res.json(channels);
-});
-app.listen(port, () => {
   console.log(
-    chalk.bgBlueBright.black(` YMF8 is listening to http://localhost:${port} `)
+    chalk.bgYellowBright.black(
+      ` ${client.user.username} online and ready to serve ${client.guilds.cache.size} servers! `
+    )
   );
+  
 });
 
 client.login(TOKEN);
-client.commands = new Collection();
-client.prefix = PREFIX;
-client.queue = new Map();
-
-loadCommands(client);
